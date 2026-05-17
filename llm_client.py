5b23060ea6e70e6d6d6998a217756c4ef5e12717
Dnyandip vadane <167987635+Dnyandip26@users.noreply.github.com>
2026-05-17 19:04:30 +0530

    fixed streamlit deployment

--- llm_client.py
+++ llm_client.py
@@ -2,28 +2,43 @@ from groq import Groq
 from sentence_transformers import SentenceTransformer
 import streamlit as st
 
+# Streamlit Secret
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
+# Models
 MODEL = "llama-3.3-70b-versatile"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 
+# Groq Client
 _client = Groq(api_key=GROQ_API_KEY)
-from sentence_transformers import SentenceTransformer
-_embedder = SentenceTransformer('data/local_model')
+
+# Embedding Model
+_embedder = SentenceTransformer(EMBEDDING_MODEL)
+
 
 def chat(prompt: str, system: str = "") -> tuple[str, int, int]:
     messages = []
+
     if system:
-        messages.append({"role": "system", "content": system})
-    messages.append({"role": "user", "content": prompt})
+        messages.append({
+            "role": "system",
+            "content": system
+        })
+
+    messages.append({
+        "role": "user",
+        "content": prompt
+    })
 
     response = _client.chat.completions.create(
         model=MODEL,
         messages=messages,
     )
+
     answer = response.choices[0].message.content
-    in_tok  = response.usage.prompt_tokens
+    in_tok = response.usage.prompt_tokens
     out_tok = response.usage.completion_tokens
+
     return answer, in_tok, out_tok
 
 
